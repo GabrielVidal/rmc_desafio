b10c22b58d103246003aee960a2ea93968ffd578
Gabriel Vidal <vidal95dev@gmail.com>
2018-05-27 20:59:09 -0300

    up

--- app/includes/head.php
+++ app/includes/head.php
@@ -8,22 +8,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="libs/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/font.css">
 </head>
 <body>
 
-<header class="container">
-	<div class="row">
-		<div class="col-xs-12 col-sm-2">
-			<a href="/"><img src="img/logo.png"></a>
-		</div>	
+<header>
+	<section class="head">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-2">
+					<a href="/"><img src="img/logo.png"></a>
+				</div>	
 
-		<nav class="col-xs-12 col-sm-10">
-			<ul>
-				<li><a href="">cardápio</a></li>
-				<li><a href="">sobre</a></li>
-				<li><a href="">contato</a></li>
-			</ul>
-		</nav>
-	</div>
+				<nav class="col-xs-12 col-sm-10">
+					<ul>
+						<li><a href="">cardápio</a></li>
+						<li><a href="">sobre</a></li>
+						<li><a href="">contato</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</section>
+	<section id="slider">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="img/banner.png" alt="First slide">
+			    </div>
+			</div>
+		</div>
+	</section>
 </header>
--- app/index.php
+++ app/index.php
@@ -1,13 +1,4 @@
 <?php include 'includes/head.php'; ?>
-<section id="slider">
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-	  <div class="carousel-inner">
-		    <div class="carousel-item active">
-		      <img class="d-block w-100" src="img/banner.png" alt="First slide">
-		    </div>
-		</div>
-	</div>
-</section>
 
 <section id="cardapio">
 	<div class="container">
